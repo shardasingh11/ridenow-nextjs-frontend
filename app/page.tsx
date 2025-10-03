9be9089fd2346de7d1ff301948a52225ea37b44a
Sharda Singh <shardasingh110011@gmail.com>
2025-10-03 16:04:10 +0530

    Booking section and features badges structure is created in main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import MainSection from "@/components/MainSection";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className="">
       <Navbar />
+      <MainSection/>
     </div>
   );
 }
